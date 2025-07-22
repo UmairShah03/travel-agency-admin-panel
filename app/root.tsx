8dcbf4689d355ec5bf0b8abbc58e712368ad8675
Umairshah03 <uishah08@gmail.com>
2025-07-22 11:26:24 +0500

    Routing

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import { registerLicense } from "@syncfusion/ej2-base";
+import pkg from "@syncfusion/ej2-base";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,6 +24,7 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+const { registerLicense } = pkg;
 registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 export function Layout({ children }: { children: React.ReactNode }) {
---
+++ app/routes/admin/AllUsers.tsx
@@ -0,0 +1,5 @@
+const AllUsers = () => {
+  return <div>All users Table</div>;
+};
+
+export default AllUsers;
